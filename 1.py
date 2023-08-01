afe4ce61efd95d03875ca35ba294e798b3caa46a
‪Aya Saied‬‏ <46829793+ayaasaied@users.noreply.github.com>
2023-08-02 01:58:13 +0300

    Basic Exercises
    37 -Write a Python program that displays your name, age, and address on three different lines.

---
+++ 1.py
@@ -0,0 +1,6 @@
+#w3resources -37. Write a Python program that displays your name,
+#age, and address on three different lines.
+name="aya"
+age=26
+address="16 sh ahmed..."
+print ("your name is:"+ name , "your age "+ str(age) , "your address" +address)

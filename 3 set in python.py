02bc1bd0629a57298efcbed8c4004a074c022fd8
‪Aya Saied‬‏ <46829793+ayaasaied@users.noreply.github.com>
2023-08-09 22:20:05 +0300

    write a python set to add and remove and know the length of the set

---
+++ 3 set in python.py
@@ -0,0 +1,11 @@
+##Python Data Types: Sets - Exercises
+#add-remove  member(s) to a set.
+setlist=set();
+setlist.add(1);
+setlist.add(30);
+setlist.add(4);
+setlist.add(6);
+print(len(setlist))
+print (setlist)
+setlist.remove(6);
+print(setlist);

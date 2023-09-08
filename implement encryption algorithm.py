3ae08e3fa5f84f8d8c8a79a8c9187e33756b042a
‪Aya Saied‬‏ <46829793+ayaasaied@users.noreply.github.com>
2023-09-08 22:53:03 +0300

    implement encryption algorithm by python

---
+++ implement encryption algorithm.py
@@ -0,0 +1,34 @@
+def caesar_encrypt(plaintext, shift):
+    ciphertext = ""
+    for char in plaintext:
+        if char.isalpha():
+            if char.isupper():
+                ciphertext += chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+            else:
+                ciphertext += chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+        else:
+            ciphertext += char
+    return ciphertext
+
+def caesar_decrypt(ciphertext, shift):
+    plaintext = ""
+    for char in ciphertext:
+        if char.isalpha():
+            if char.isupper():
+                plaintext += chr((ord(char) - ord('A') - shift) % 26 + ord('A'))
+            else:
+                plaintext += chr((ord(char) - ord('a') - shift) % 26 + ord('a'))
+        else:
+            plaintext += char
+    return plaintext
+
+# Example usage
+plaintext = "Hello, world!"
+shift = 3
+
+ciphertext = caesar_encrypt(plaintext, shift)
+decrypted_text = caesar_decrypt(ciphertext, shift)
+
+print("Plaintext:", plaintext)
+print("Ciphertext:", ciphertext)
+print("Decrypted text:", decrypted_text)
